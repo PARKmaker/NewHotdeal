ff74bdf45327a557436d9845fe873cbf8c0cc9b7
PARKmaker <tkdgus8903@gmail.com>
2023-11-03 02:09:49 +0900

    테스트

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,4 +1,5 @@
-// Import the functions you need from the SDKs you need
+console.log("하이");
+
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, goOffline } from "firebase/database";
 import getInfo from "./dom.js";
