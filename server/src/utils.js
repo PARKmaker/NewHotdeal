e004ab422fec47aef89982acdd689f795ac0f151
PARKmaker <tkdgus8903@gmail.com>
2023-11-03 23:00:21 +0900

    Feat, 분 붙여 출력

--- server/src/utils.js
+++ server/src/utils.js
@@ -2,11 +2,18 @@ const today = new Date();
 const tempDay = today.getDate().toString();
 const tempmonth = (today.getMonth() + 1).toString();
 const tempHour = today.getHours().toString();
+const tempMinute = (today.getMinutes() + 1).toString();
+
+function dateConcatZero(str) {
+  return str < 10 ? "0" + str : str;
+}
 
 const year = today.getFullYear().toString();
-const day = tempDay < 10 ? "0" + tempDay : tempDay;
-const month = tempmonth < 10 ? "0" + tempmonth : tempmonth;
-const hour = tempHour < 10 ? "0" + tempHour : tempHour;
-const curDate = year + month + day + hour;
+const day = dateConcatZero(tempDay);
+const month = dateConcatZero(tempmonth);
+const hour = dateConcatZero(tempHour);
+const minute = dateConcatZero(tempMinute);
+
+const curDate = year + month + day + hour + minute;
 
 export default curDate;
